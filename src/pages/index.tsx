ce1c6c5cd555dfbc690078f483862b7e5edabf8b
João Pedro Basso Audibert <hotsjoao@gmail.com>
2021-02-20 17:03:23 -0300

    adding server side rendering

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,35 +1,39 @@
 import { useEffect, useState } from "react";
 import { Title } from "../styles/pages/Home";
 import api from '../services/api';
+import { GetServerSideProps } from "next";
 
 interface IRecommendedProducts{
   id: string;
   title: string;
 }
 
-export default function Home() {
-  const [recommendedProducts, setRecommendedProducts] = useState<IRecommendedProducts[]>([]);
+interface IHomeProps {
+  recommendedProducts: IRecommendedProducts[];
+}
 
-  useEffect(() => {
-    async function loadRecommendedProducts() {
-      const response = await api.get('recommended');
-
-      setRecommendedProducts(response.data)
-    }
-
-    loadRecommendedProducts();
-  }, [])
-
+export default function Home({ recommendedProducts }:IHomeProps) {
   return (
     <div>
       <Title>Hello World</Title>
       <ul>
-          {recommendedProducts.map(recommendedProducts => {
+          {recommendedProducts.map(recommendedProduct => {
             return (
-              <li key={recommendedProducts.id}>{recommendedProducts.title}</li>
+              <li key={recommendedProduct.id}>{recommendedProduct.title}</li>
             )
           })}
       </ul>
     </div>
   )
+}
+
+export const getServerSideProps: GetServerSideProps<IHomeProps> = async () => {
+  const response = await api.get('recommended');
+  const recommendedProducts = await response.data;
+
+  return {
+    props: {
+      recommendedProducts
+    }
+  }
 }
