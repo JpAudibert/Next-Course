5481c08880b17a451a9a90b4fc100f9416f15d48
João Pedro Basso Audibert <hotsjoao@gmail.com>
2021-02-20 13:29:23 -0300

    eslints and som client side fetching

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,35 @@
+import { useEffect, useState } from "react";
 import { Title } from "../styles/pages/Home";
+import api from '../services/api';
+
+interface IRecommendedProducts{
+  id: string;
+  title: string;
+}
 
 export default function Home() {
+  const [recommendedProducts, setRecommendedProducts] = useState<IRecommendedProducts[]>([]);
+
+  useEffect(() => {
+    async function loadRecommendedProducts() {
+      const response = await api.get('recommended');
+
+      setRecommendedProducts(response.data)
+    }
+
+    loadRecommendedProducts();
+  }, [])
+
   return (
-    <Title>Hello World</Title>
+    <div>
+      <Title>Hello World</Title>
+      <ul>
+          {recommendedProducts.map(recommendedProducts => {
+            return (
+              <li key={recommendedProducts.id}>{recommendedProducts.title}</li>
+            )
+          })}
+      </ul>
+    </div>
   )
 }
